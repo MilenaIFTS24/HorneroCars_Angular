78bdc898535134f1aff23ebf9ca3c954cae72975
Milena Lingua <lingua.milena@gmail.com>
2025-06-14 21:59:33 -0300

    HTML y CSS de tarifas. Acomodo tarjetas y agrego contenido de texto. Error no resuelto al querer insertar imágenes en las tarjetas.

--- src/app/componentes/publico/informacion/informacion.component.ts
+++ src/app/componentes/publico/informacion/informacion.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TarifasComponent } from './tarifas/tarifas.component';
 
 @Component({
   selector: 'app-informacion',
-  imports: [],
+  imports: [TarifasComponent],
   templateUrl: './informacion.component.html',
   styleUrl: './informacion.component.css'
 })
