b3053fdbf952ba1d67ed7831183b2f4d491e338a
Julian <35359714@ifts24.edu.ar>
2025-06-19 20:30:36 -0300

    sesion y modal funcionando

--- src/app/componentes/autenticacion/login/login.component.ts
+++ src/app/componentes/autenticacion/login/login.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { LoginService } from '../../../servicios/login.service';
+import { User } from '../../../modelos/user';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+ userId: string = '';
+  password: string = '';
+  error: string = '';
 
+  constructor(
+    private loginService: LoginService,
+    public activeModal: NgbActiveModal
+  ) {}
+
+  iniciarSesion() {
+    this.loginService.getUsuarios().subscribe((usuarios: User[]) => {
+      const user = usuarios.find(
+        u => u.userId === this.userId && u.password === this.password
+      );
+
+      if (user) {
+        sessionStorage.setItem('usuario', JSON.stringify(user));
+        this.activeModal.close(user);
+      } else {
+        this.error = 'Usuario o contraseña incorrectos';
+      }
+    });
+  }
+
+  cerrarModal() {
+    this.activeModal.dismiss();
+  }
 }
--- src/app/servicios/login.service.ts
+++ src/app/servicios/login.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../modelos/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private url = 'assets/usuarios.json';
+ constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getUsuarios(): Observable<User[]> {
+    return this.http.get<User[]>(this.url);
+  }
 }
