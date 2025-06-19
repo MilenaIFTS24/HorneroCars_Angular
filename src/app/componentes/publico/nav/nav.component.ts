b3df48bec26ca8b70a70f0d2997b871fed7ac640
Julian <35359714@ifts24.edu.ar>
2025-06-18 23:47:05 -0300

    Nav base

--- src/app/componentes/publico/nav/nav.component.ts
+++ src/app/componentes/publico/nav/nav.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { NgbNavModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [NgbNavModule, NgbDropdownModule, RouterModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
-
-}
+export class NavComponent {}
