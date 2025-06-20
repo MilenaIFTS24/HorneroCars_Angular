f09bacaf18a00bebe9614ddd2c06bdc9f6d0d821
Julian <35359714@ifts24.edu.ar>
2025-06-20 17:45:27 -0300

    sesion-servicios comentado

--- src/app/servicios/login.service.ts
+++ src/app/servicios/login.service.ts
@@ -1,15 +1,21 @@
+// Importaciones necesarias para el servicio
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../modelos/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Hace que el servicio esté disponible a nivel global
 })
 export class LoginService {
-  private url = 'assets/usuarios.json';
- constructor(private http: HttpClient) {}
 
+  // Ruta del archivo JSON local con los usuarios (simulando una API)
+  private url = 'assets/usuarios.json';
+
+  // Inyección de HttpClient para realizar peticiones HTTP
+  constructor(private http: HttpClient) {}
+
+  // Método que retorna la lista de usuarios como observable
   getUsuarios(): Observable<User[]> {
     return this.http.get<User[]>(this.url);
   }
