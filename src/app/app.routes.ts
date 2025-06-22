4bf203b75d176c63d841d2b1308267a750e7a1f4
Liliana Alvarez <lialvarez0630@gmail.com>
2025-06-22 18:50:10 -0300

    Rutas Actualizadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,10 +14,8 @@ export const routes: Routes = [ //rutas elaboradas con lazy loading.
     { path: "registro", loadComponent: () => import('./componentes/autenticacion/registro/registro.component').then(m => RegistroComponent) },
     { path: "dashboardAdmin", loadComponent: () => import('./componentes/privado/admin/dashboard-admin/dashboard-admin.component').then(m => DashboardAdminComponent) },
     { path: "dashboardUser", loadComponent: () => import('./componentes/privado/user/dashboard-user/dashboard-user.component').then(m => DashboardUserComponent) },
-    { path: "contacto", loadComponent: () => import('./componentes/publico/informacion/contacto/contacto.component').then(m => ContactoComponent) },
     { path: "informacion", loadComponent: () => import('./componentes/publico/informacion/informacion.component').then(m => InformacionComponent) },
     { path: "reserva", loadComponent: () => import('./componentes/publico/reserva/reserva.component').then(m => ReservaComponent) },
-    { path: "vehiculos", loadComponent: () => import('./componentes/publico/informacion/vehiculos/vehiculos.component').then(m => m.VehiculosComponent) },
-
+    
     { path: '**', redirectTo: '/' } //en caso de no existir la ruta escrita, redirige al home.
 ];
--- src/app/componentes/publico/informacion/informacion.component.ts
+++ src/app/componentes/publico/informacion/informacion.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactoComponent } from "./contacto/contacto.component";
+import { VehiculosComponent } from "./vehiculos/vehiculos.component";
 
 @Component({
   selector: 'app-informacion',
-  imports: [],
+  imports: [ContactoComponent, VehiculosComponent],
   templateUrl: './informacion.component.html',
   styleUrl: './informacion.component.css'
 })
