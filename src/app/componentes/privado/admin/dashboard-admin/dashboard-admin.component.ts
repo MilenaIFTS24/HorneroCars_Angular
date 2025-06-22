4bcac8caa63f2127d41b1ec60665cb9860138e6f
Julian <35359714@ifts24.edu.ar>
2025-06-22 16:42:44 -0300

    dashboardAdmin

--- src/app/componentes/privado/admin/dashboard-admin/dashboard-admin.component.ts
+++ src/app/componentes/privado/admin/dashboard-admin/dashboard-admin.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+interface NavItem {
+  label: string;
+  icon: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-dashboard-admin',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './dashboard-admin.component.html',
   styleUrl: './dashboard-admin.component.css'
 })
-export class DashboardAdminComponent {
+export class DashboardAdminComponent implements OnInit {
+
+  userId: string = '';
+
+  navItems: NavItem[] = [
+    { label: 'Inicio', icon: 'fas fa-home', link: 'home' },
+    { label: 'Mi Cuenta', icon: 'fas fa-circle-user', link: 'mi-cuenta' },
+    { label: 'Mis Reservas', icon: 'fas fa-suitcase', link: 'mis-reservas' },
+    { label: 'Hacer Reserva', icon: 'fa-solid fa-car', link: 'hacer-reserva' },
+    { label: 'Configuración', icon: 'fas fa-cog', link: 'configuracion' }
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const usuarioString = sessionStorage.getItem('usuario');
+    if (usuarioString) {
+      const usuario = JSON.parse(usuarioString);
+      this.userId = usuario.userId;
+    }
+  }
 
 }
