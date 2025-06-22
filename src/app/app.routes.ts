66a29aba637e939561b1f8b4eb9b1f0194c59291
brenda-ncc <37358704@ifts24.edu.ar>
2025-06-21 21:35:05 -0300

    base dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,13 +9,33 @@ import { InformacionComponent } from './componentes/publico/informacion/informac
 import { ReservaComponent } from './componentes/publico/reserva/reserva.component';
 
 export const routes: Routes = [ //rutas elaboradas con lazy loading.
-    { path: "", loadComponent: () => import('./componentes/publico/home/home.component').then(m => HomeComponent) },
-    { path: "login", loadComponent: () => import('./componentes/autenticacion/login/login.component').then(m => LoginComponent) },
-    { path: "registro", loadComponent: () => import('./componentes/autenticacion/registro/registro.component').then(m => RegistroComponent) },
-    { path: "dashboardAdmin", loadComponent: () => import('./componentes/privado/admin/dashboard-admin/dashboard-admin.component').then(m => DashboardAdminComponent) },
-    { path: "dashboardUser", loadComponent: () => import('./componentes/privado/user/dashboard-user/dashboard-user.component').then(m => DashboardUserComponent) },
-    { path: "contacto", loadComponent: () => import('./componentes/publico/informacion/contacto/contacto.component').then(m => ContactoComponent) },
-    { path: "informacion", loadComponent: () => import('./componentes/publico/informacion/informacion.component').then(m => InformacionComponent) },
-    { path: "reserva", loadComponent: () => import('./componentes/publico/reserva/reserva.component').then(m => ReservaComponent) },
-    { path:'**', redirectTo: '/' } //en caso de no existir la ruta escrita, redirige al home.
+  { path: "", loadComponent: () => import('./componentes/publico/home/home.component').then(m => HomeComponent) },
+  { path: "login", loadComponent: () => import('./componentes/autenticacion/login/login.component').then(m => LoginComponent) },
+  { path: "registro", loadComponent: () => import('./componentes/autenticacion/registro/registro.component').then(m => RegistroComponent) },
+  { path: "dashboardAdmin", loadComponent: () => import('./componentes/privado/admin/dashboard-admin/dashboard-admin.component').then(m => DashboardAdminComponent) },
+  { path: "contacto", loadComponent: () => import('./componentes/publico/informacion/contacto/contacto.component').then(m => ContactoComponent) },
+  { path: "informacion", loadComponent: () => import('./componentes/publico/informacion/informacion.component').then(m => InformacionComponent) },
+  { path: "reserva", loadComponent: () => import('./componentes/publico/reserva/reserva.component').then(m => ReservaComponent) },
+
+  {
+    path: "dashboardUser",
+    loadComponent: () =>
+      import('./componentes/privado/user/dashboard-user/dashboard-user.component')
+        .then(m => m.DashboardUserComponent),
+    children: [
+      {
+        path: "home",
+        loadComponent: () =>
+          import('./componentes/privado/user/dashboard-user/home-dashboard-user/home-dashboard-user.component')
+            .then(m => m.HomeDashboardUserComponent)
+      },
+      {
+        path: "",
+        redirectTo: "home",
+        pathMatch: "full"
+      }
+    ]
+  },
+
+  { path: '**', redirectTo: '/' } 
 ];
--- src/app/componentes/privado/user/dashboard-user/dashboard-user.component.ts
+++ src/app/componentes/privado/user/dashboard-user/dashboard-user.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+// sidebar.component.ts
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomeDashboardUserComponent } from './home-dashboard-user/home-dashboard-user.component';
+
+
+
+interface NavItem {
+  label: string;
+  icon: string; // Clase CSS para el icono
+  link: string;
+}
 
 @Component({
-  selector: 'app-dashboard-user',
-  imports: [],
+  selector: 'app-sidebar',
   templateUrl: './dashboard-user.component.html',
-  styleUrl: './dashboard-user.component.css'
+  styleUrls: ['./dashboard-user.component.css'], 
+  imports: [RouterModule, CommonModule]
 })
-export class DashboardUserComponent {
+export class DashboardUserComponent implements OnInit {
+ navItems: NavItem[] = [
+  { label: 'Inicio', icon: 'fas fa-home', link: 'home' },
+  { label: 'Mi Cuenta', icon: 'fas fa-circle-user', link: 'mi-cuenta' },
+  { label: 'Mis Reservas', icon: 'fas fa-suitcase', link: 'mis-reservas' },
+  { label: 'Hacer Reserva', icon: 'fa-solid fa-car', link: 'hacer-reserva' },
+  { label: 'Configuración', icon: 'fas fa-cog', link: 'configuracion' }
+];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
+
+
