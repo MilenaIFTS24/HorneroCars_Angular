02eb19e7f990ce984b1296f47e91bc3a8aacacf4
brenda-ncc <37358704@ifts24.edu.ar>
2025-06-25 04:11:19 -0300

    se arregla validacion de caracteres de userid dentro de registro

--- src/app/componentes/autenticacion/registro/registro.component.ts
+++ src/app/componentes/autenticacion/registro/registro.component.ts
@@ -25,7 +25,7 @@ export class RegistroComponent implements OnInit {
 
   ngOnInit(): void {
     this.registroForm = this.fb.group({
-      userId: ['', [Validators.required, Validators.minLength(5)]],
+      userId: ['', [Validators.required, Validators.minLength(3)]],
       nombre: ['', [Validators.required, Validators.minLength(2)]],
       apellido: ['', [Validators.required, Validators.minLength(2)]],
       fechaNacimiento: ['', [Validators.required]],
